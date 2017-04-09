211abd833250fe0020d4269defe08e7e1fcb1fa6
Justin Silvestre <justin.silvestre@copart.com>
2017-04-09 13:49:20 -0500

    Seed EnLemma and EnPartOfSpeech

--- web/models/en_lemma.ex
+++ web/models/en_lemma.ex
@@ -3,7 +3,7 @@ defmodule Lipu.EnLemma do
 
   schema "en_lemmas" do
     field :text, :string
-    belongs_to :pos, Lipu.Pos
+    belongs_to :pos, Lipu.EnPartOfSpeech
 
     timestamps()
   end
@@ -13,7 +13,7 @@ defmodule Lipu.EnLemma do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:text])
-    |> validate_required([:text])
+    |> cast(params, [:text, :pos_id])
+    |> validate_required([:text, :pos_id])
   end
 end
