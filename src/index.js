5a82326a2ea1e55398f231079ac5e7deafcaebe3
Justin Silvestre <justin.silvestre@copart.com>
2017-04-09 16:31:11 -0500

    Set up TranslateChannel

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import { Provider } from 'react-redux'
 import rxjs from 'rxjs' // eslint-disable-line no-unused-vars
 import App from './components/App'
 import store from './redux'
+import socket from './utils/socket'
+
+socket.connect()
+const channel = socket.channel('translate:' + 42)
+channel.on('ping', (o) => console.log("PING!", o.count, o))
+channel.join()
+  .receive('ok', (r) => {
+    console.log('JOIN WORKEDDED???! :OOO')
+  })
+  .receive('error', reason => console.log('join failed :(', reason))
 
 ReactDOM.render(
   <Provider store={store}>
