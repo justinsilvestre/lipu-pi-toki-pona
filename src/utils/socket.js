4dae1773bfb8b9a02dcba54aa75910f6fc6aef49
Justin Silvestre <justinsilvestre@gmail.com>
2017-04-22 23:53:53 -0500

    Remove logging

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -3,8 +3,8 @@ import { Socket } from 'phoenix'
 const opts = {
   params: { token: window.userToken },
 }
-if (process.env.NODE_ENV === 'development')
-  opts.logger = (kind, msg, data) => console.log(kind, msg, data)
+// if (process.env.NODE_ENV === 'development')
+//   opts.logger = (kind, msg, data) => console.log(kind, msg, data)
 
 const socket = new Socket('/socket', opts)
 
