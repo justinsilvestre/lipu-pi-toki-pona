73feb2aebb3ffc77dc2a4ca45b81c8eda2e8182c
Justin Silvestre <justinsilvestre@gmail.com>
2017-04-22 23:47:46 -0500

    Add analytics and connect to socket immediately like before

--- src/utils/channel.js
+++ src/utils/channel.js
@@ -2,6 +2,8 @@ import socket from './socket'
 
 const channel = socket.channel('translate:' + 42)
 
+socket.connect()
+
 export function pull(pushEvent, payload) {
   return new Promise((resolve, reject) => {
       channel.push(pushEvent, payload)
